c7d66f61e7052c3de98eafea3fd13db5dab0a5ec
Sara Rattenbury <sararatts@gmail.com>
2022-03-31 21:04:24 -0300

    created functions, added dynamic grid rows and columns

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 let gridSize = 16;
 
 const divGridContainer = document.querySelector(".grid-container");
-//const divGrid = document.createElement('div');
+
 createGrid(gridSize);
 
-const gridPieces = document.querySelectorAll('.div-grid');
-
-gridPieces.forEach((piece) => {
-    piece.addEventListener('mouseover', () => {
-        piece.classList.add('color');
-    });
-});
-
 const resetButton = document.getElementById("reset");
-resetButton.addEventListener('click', () => {
+resetButton.addEventListener('click', resetGrid);
+
+function createGrid(size) {
+
+    divGridContainer.style.gridTemplateRows = `repeat(${size}, auto)`;
+    divGridContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    //grid-template-columns: repeat(16, auto);
+    //grid-template-rows: repeat(16, auto);
+
+    for (let index = 0; index < size*size; index++) {
+        const divGrid = document.createElement('div');
+        //divGrid.textContent = "js "+index;
+        divGrid.classList.add('div-grid');
+        //divGrid.style.height = '50px';
+        //divGrid.style.width = '50px';
+        divGridContainer.appendChild(divGrid);
+    }
+
+    const gridPieces = document.querySelectorAll('.div-grid');
+
+    gridPieces.forEach((piece) => {
+        piece.addEventListener('mouseover', () => {
+            piece.classList.add('color');
+        });
+    });
+}
+
+function resetGrid(){
+    const gridPieces = document.querySelectorAll('.div-grid');
+
     gridPieces.forEach((piece) => {
         piece.classList.remove('color')
     });
 
-    let newGrid = window.prompt("Enter grid size: ");
-    console.log(newGrid);   
-    //createGrid(newGrid);
-});
-
-function createGrid(size) {
-
-    for (let index = 0; index < size*size; index++) {
-        const divGrid = document.createElement('div');
-        //divGrid.textContent = "JS Test" + index;
-        divGrid.classList.add('div-grid');
-        divGridContainer.appendChild(divGrid);
+    //let element = document.getElementById("top");
+    while (divGridContainer.firstChild) {
+        divGridContainer.removeChild(divGridContainer.firstChild);
     }
+
+    let newGrid = window.prompt("Enter grid size:");
+    console.log(newGrid);
+    
+    if (newGrid <= 100 && newGrid >= 16)
+        createGrid(newGrid);
+    else 
+        createGrid(16);
 
 }
